d99c41afda3a15676371f11268300696defe8e40
Kenny Mui <klmui@wisc.edu>
2020-01-13 23:58:58 -0600

    Add mime-type

--- src/app/posts/post-create/mime-type.validator.ts
+++ src/app/posts/post-create/mime-type.validator.ts
@@ -1,3 +1,5 @@
+// Validate an img (JPG or PNG)
+
 import { AbstractControl } from '@angular/forms';
 import { Observable, Observer } from 'rxjs';
 
@@ -6,11 +8,37 @@ export const mimeType = (control: AbstractControl): Promise<{ [key: string]: any
   const file = control.value as File;
   const fileReader = new FileReader();
   // Needs to return an observable or promise
-  const frObs = Observable.create((observer: Observer) => {
+  const frObs = Observable.create((observer: Observer<{ [key: string]: any }>) => {
     // loadend has more info than 'load'
-    fileReader.addEventListener("loadend", () => {
+    fileReader.addEventListener('loadend', () => {
       // Do the mime-type validation
-      
+
+      // Parse mime-type with 8 bit unsigned array
+      // Look into jpg file and make sure that it's actually jpg
+      // Infer filetype
+      const arr = new Uint8Array(fileReader.result as ArrayBuffer).subarray(0, 4);
+
+      let header = "";
+      // Look into pattern
+      for (let i = 0; i < arr.length; i++) {
+        header += arr[i].toString(16); // Convert to hex string
+      }
+      // Check for certain patterns
+      switch (header) {
+        case "89504e47":
+          isValid = true;
+          break;
+        case "ffd8ffe0":
+        case "ffd8ffe1":
+        case "ffd8ffe2":
+        case "ffd8ffe3":
+        case "ffd8ffe8":
+          isValid = true;
+          break;
+        default:
+          isValid = false; // Or you can use the blob.type as fallback
+          break;
+      }
     });
     fileReader.readAsArrayBuffer(file); // Access the mime-type
   });
